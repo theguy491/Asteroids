820f0fba6c82940055b95ff427e518dff058279b
TheGuy491 <tech@tkeener.com>
2026-01-09 19:28:56 -0500

    Created player icon and added refresh rate

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 import pygame
-from constants import SCREEN_HEIGHT, SCREEN_WIDTH
+from constants import SCREEN_HEIGHT, SCREEN_WIDTH, PLAYER_RADIUS
 from logger import log_state
+from player import Player
 
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    x = SCREEN_WIDTH/2
+    y = SCREEN_HEIGHT/2
+    player = Player(x,y)
 
     while True:
         log_state()
@@ -13,8 +19,11 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-
+            
+        clock.tick(60)
+        dt = clock.tick(60)
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
 
 
